760a4c0e0b662d168ec820434981037ded50c222
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-02-04 14:59:14 +0200

    Add files via upload

---
+++ tema 16-cp5/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Пример использования класса
+            WebsiteVisit visit1 = new WebsiteVisit("example.com", "192.168.1.1", "Chrome", DateTime.Now, new TimeSpan(10, 30, 0));
+            WebsiteVisit visit2 = new WebsiteVisit("example.com", "192.168.1.2", "Firefox", DateTime.Now, new TimeSpan(15, 45, 0));
+
+            // Вывод информации о посещениях
+            visit1.DisplayInfo();
+            Console.WriteLine();
+            visit2.DisplayInfo();
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ tema 16-cp5/ConsoleApp1/WebsiteVisit.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class WebsiteVisit
+    {
+
+        /// <summary>
+        /// адрес открытой web-странички;
+        /// </summary>
+        private string PageAddress;
+
+        /// <summary>
+        /// ip-адрес посетителя
+        /// </summary>
+        private string ipAddress;
+
+        /// <summary>
+        /// web-браузер посетителя
+        /// </summary>
+        private string webBrowser;
+
+        /// <summary>
+        /// дата визита
+        /// </summary>
+        private DateTime visitDate;
+
+        /// <summary>
+        /// время визита
+        /// </summary>
+        private TimeSpan visitTime;
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="pageAddress"></param>
+        /// <param name="ipAddress"></param>
+        /// <param name="webBrowser"></param>
+        /// <param name="visitDate"></param>
+        /// <param name="visitTime"></param>
+        public WebsiteVisit(string pageAddress, string ipAddress, string webBrowser, DateTime visitDate, TimeSpan visitTime)
+        {
+            PageAddress = pageAddress;
+            IpAddress = ipAddress;
+            WebBrowser = webBrowser;
+            VisitDate = visitDate;
+            VisitTime = visitTime;
+        }
+
+
+        public string PageAddress1 { get { return PageAddress; } set { PageAddress = value; } }
+        public string IpAddress { get { return ipAddress; } set { ipAddress = value; } }
+        public string WebBrowser { get { return webBrowser; } set { webBrowser = value; } }
+        public DateTime VisitDate { get { return visitDate; } set { visitDate = value; } }
+        public TimeSpan VisitTime { get { return visitTime; } set { visitTime = value; } }
+
+
+        /// <summary>
+        /// Метод для вывода информации о посещении
+        /// </summary>
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Адрес страницы: {PageAddress}");
+            Console.WriteLine($"IP-адрес посетителя: {IpAddress}");
+            Console.WriteLine($"Web-браузер посетителя: {WebBrowser}");
+            Console.WriteLine($"Дата визита: {VisitDate.ToShortDateString()}");
+            Console.WriteLine($"Время визита: {VisitTime.ToString(@"hh\:mm\:ss")}");
+        }
+
+
+
+
+
+    }
+}
